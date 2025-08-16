5477a6e0732531f067e9d13f99d73481873fb02e
SHAHAD SAAD <ShahadSaad001.dev@gmail.com>
2025-08-16 19:01:02 +0300

    chores: generate and push updated swagger.json to docs repo

---
+++ scripts/generate-swagger.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+
+async function generateSwagger() {
+    const app = await NestFactory.create(AppModule, { logger: false });
+
+    const config = new DocumentBuilder()
+        .setTitle('Silah Backend API Documentation')
+        .setDescription(
+            'Use this documentation to explore, test, and understand the available API endpoints, their request/response structure, and any required parameters such as headers, cookies, or authentication tokens.',
+        )
+        .setVersion('1.0')
+        .addCookieAuth('token')
+        .addSecurity('cookie', {
+            type: 'apiKey',
+            in: 'cookie',
+            name: 'token',
+        })
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    fs.writeFileSync('./docs/swagger.json', JSON.stringify(document, null, 2));
+    console.log('Swagger JSON generated successfully!');
+    await app.close();
+}
+
+generateSwagger();
