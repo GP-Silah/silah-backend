042d1899bae1dd194f3536b227528fee3ef6ad5d
SHAHAD SAAD <ShahadSaad001.dev@gmail.com>
2025-09-29 08:47:48 +0300

    Add cart structure and save git from corrupt

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,9 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('Cart')
-@Controller('cart')
+@ApiTags('Carts')
+@Controller('carts')
 export class CartController {
     constructor(private readonly cartService: CartService) {}
+
+    @Get(':buyerId')
+    async getBuyerActiveCart() {}
+
+    // auto creation when first item is added
+    @Post('/items')
+    async addItem() {}
+
+    @Patch(':cartId/items/:itemId')
+    async updateItemQuantity() {}
+
+    @Delete(':cartId/items/:itemId')
+    async removeItem() {}
+
+    @Delete(':cartId')
+    async clearCart() {}
+
+    @Delete(':cartId/suppliers/:supplierId')
+    async removeSupplierFromCart() {}
+
+    // internally creates multiple orders
+    @Post(':cartId/checkout')
+    async checkoutCart() {}
 }
